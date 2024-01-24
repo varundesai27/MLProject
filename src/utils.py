13403675d096d0780f1dea85b76e65bba925a3d7
varundesai27 <varunkumardesai99@gmail.com>
2024-01-24 17:29:02 +0530

    Hyper Parameter Tuning is done

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ from src.exception import CustomException
 import sys
 from pickle4 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,13 +18,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try: 
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
